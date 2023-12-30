bae6d0a4a1dd2d51578552c17a0fc027bed26bf5
bsb0102 <bsbrandon0102@gmail.com>
2023-12-30 23:36:59 +0000

    linux change

---
+++ frontend/build/precache-manifest.00d453d03a59f196bb147e7605245792.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07cf6c8a71893a8b4e9469de81fae1c9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b5228bf9257c7ec9dc2e",
+    "url": "/static/css/main.e16865cc.chunk.css"
+  },
+  {
+    "revision": "ec6cb44a4d2d2b303495",
+    "url": "/static/js/2.ab57b840.chunk.js"
+  },
+  {
+    "revision": "92d7e5936c3f0978605df3eb8b77e202",
+    "url": "/static/js/2.ab57b840.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5228bf9257c7ec9dc2e",
+    "url": "/static/js/main.4d9fb2ec.chunk.js"
+  },
+  {
+    "revision": "0b6b46ef1e17d32e6df4",
+    "url": "/static/js/runtime-main.bce919bb.js"
+  }
+]);
--- server.js
+++ server.js
@@ -1,17 +1,21 @@
 const express = require('express');
-const mongoose = require('mongoose'); // Import Mongoose
+const mongoose = require('mongoose');
 const cors = require('cors');
 const path = require('path');
+const fs = require('fs'); // Require the 'fs' module to read the certificate files
 const errorHandler = require('./backend/middleware/errorHandler');
 const authenticateToken = require('./backend/middleware/authenticateToken');
+const https = require('https'); // Require the 'https' module
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Connect to MongoDB (adjust the connection string and options as needed)
-mongoose.connect('mongodb://localhost/mydatabase', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
+mongoose
+  .connect('mongodb://localhost/mydatabase', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => {
     console.log('MongoDB connected');
   })
@@ -41,6 +45,37 @@ app.get('*', (req, res) => {
 
 app.use(errorHandler);
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+const privateKey = fs.readFileSync('key.pem', 'utf8');
+const certificate = fs.readFileSync('cert.pem', 'utf8');
+const credentials = { key: privateKey, cert: certificate };
+
+// Create an HTTPS server
+const httpsServer = https.createServer(credentials, app);
+
+http.createServer((req, res) => {
+  res.writeHead(301, { Location: `https://${req.headers.host}${req.url}` });
+  res.end();
+}).listen(80);
+
+const options = {
+  key: fs.readFileSync('/etc/nginx/ssl/nginx-selfsigned.key'),
+  cert: fs.readFileSync('/etc/nginx/ssl/nginx-selfsigned.crt'),
+};
+
+https.createServer(options, app).listen(443);
+
+httpsServer.listen(PORT, () => {
+  console.log(`HTTPS Server running on port ${PORT}`);
+  console.log(`URL: https://localhost:${PORT}`);
+});
+
+// Handle process termination
+process.on('SIGINT', () => {
+  httpsServer.close(() => {
+    console.log('HTTPS Server terminated.');
+    mongoose.connection.close(false, () => {
+      console.log('MongoDB connection closed.');
+      process.exit(0);
+    });
+  });
 });
